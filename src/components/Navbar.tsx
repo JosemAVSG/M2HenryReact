4f67f6f81a9628a26c115e2ee176e2c30cc66960
Jose Miguel JGTECH <jmgg.95n@gmail.com>
2024-04-05 22:31:05 -0500

    api peli

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import style from '../styles.module.css'
+
+
+const Navbar = ()=>{
+
+    return (
+        <>
+        <nav className={style.navbar}>
+            <ul>
+                <li>
+                   <a></a>
+                </li>
+                <li>
+                    
+                </li>
+            </ul>
+        </nav>
+        </>
+    )
+
+}
+
+export default Navbar
